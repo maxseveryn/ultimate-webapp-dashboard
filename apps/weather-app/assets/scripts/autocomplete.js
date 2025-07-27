e30067a5e49d4c3fd72df9c0b20b5ea7f7b6a1e4
maxseveryn <severinmaksim055@gmail.com>
2025-07-27 19:25:40 +0300

    Updated autocomplete.js

---
+++ apps/weather-app/assets/scripts/autocomplete.js
@@ -0,0 +1,121 @@
+async function getCities(query) {
+  const url = `https://wft-geo-db.p.rapidapi.com/v1/geo/cities?limit=10&namePrefix=${query}`;
+
+  const options = {
+    method: "GET",
+    headers: {
+      "X-RapidAPI-Key": API_CONFIG.RAPIDAPI_KEY,
+      "X-RapidAPI-Host": API_CONFIG.RAPIDAPI_HOST,
+    },
+  };
+
+  try {
+    const response = await fetch(url, options);
+    const data = await response.json();
+
+    console.log(data);
+
+    const cities = data.data
+      ? data.data.map((city) => `${city.name}, ${city.country}`)
+      : [];
+
+    return cities;
+  } catch (error) {
+    console.log("Err cities", error);
+    return [];
+  }
+}
+
+function autocomplete(inp, arr) {
+  var currentFocus;
+
+  inp.addEventListener("input", function (e) {
+    var a,
+      b,
+      i,
+      val = this.value;
+
+    closeAllLists();
+    if (!val) {
+      return false;
+    }
+    currentFocus = -1;
+
+    a = document.createElement("DIV");
+    a.setAttribute("id", this.id + "autocomplete-list");
+    a.setAttribute("class", "autocomplete-items");
+
+    this.parentNode.appendChild(a);
+
+    for (i = 0; i < arr.length; i++) {
+      if (arr[i].substr(0, val.length).toUpperCase() == val.toUpperCase()) {
+        b = document.createElement("DIV");
+
+        b.innerHTML = "<strong>" + arr[i].substr(0, val.length) + "</strong>";
+        b.innerHTML += arr[i].substr(val.length);
+
+        b.innerHTML += "<input type='hidden' value='" + arr[i] + "'>";
+
+        b.addEventListener("click", function (e) {
+          inp.value = this.getElementsByTagName("input")[0].value;
+
+          closeAllLists();
+        });
+        a.appendChild(b);
+      }
+    }
+  });
+
+  inp.addEventListener("keydown", function (e) {
+    var x = document.getElementById(this.id + "autocomplete-list");
+    if (x) x = x.getElementsByTagName("div");
+    if (e.keyCode == 40) {
+      currentFocus++;
+
+      addActive(x);
+    } else if (e.keyCode == 38) {
+      currentFocus--;
+
+      addActive(x);
+    } else if (e.keyCode == 13) {
+      e.preventDefault();
+      if (currentFocus > -1) {
+        if (x) x[currentFocus].click();
+      }
+    }
+  });
+  function addActive(x) {
+    if (!x) return false;
+    removeActive(x);
+    if (currentFocus >= x.length) currentFocus = 0;
+    if (currentFocus < 0) currentFocus = x.length - 1;
+    x[currentFocus].classList.add("autocomplete-active");
+  }
+  function removeActive(x) {
+    for (var i = 0; i < x.length; i++) {
+      x[i].classList.remove("autocomplete-active");
+    }
+  }
+  function closeAllLists(elmnt) {
+    var x = document.getElementsByClassName("autocomplete-items");
+    for (var i = 0; i < x.length; i++) {
+      if (elmnt != x[i] && elmnt != inp) {
+        x[i].parentNode.removeChild(x[i]);
+      }
+    }
+  }
+  document.addEventListener("click", function (e) {
+    closeAllLists(e.target);
+  });
+}
+
+const input = document.getElementById("search-input");
+
+input.addEventListener("input", async () => {
+  console.log("ggigag");
+  const query = input.value.trim();
+  if (query.length > 1) {
+    const cities = await getCities(query);
+    autocomplete(input, cities);
+  }
+});
